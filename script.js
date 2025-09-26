94481fd646b34f7c473f506bd1403486f2270491
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-26 12:21:18 +0200

    working on show all pokemon

--- script.js
+++ script.js
@@ -17,19 +17,35 @@ async function loadPokemon() {
 };
 
 
-function getInfo () {
-    
-    document.getElementById('pokemon').innerHTML = `Name Pokemon: ${allPoke[0].name}<br>`;
-    document.getElementById('pokemon').innerHTML += `ID Pokemon: ${allPoke[0].id}<br>`;
-    document.getElementById('pokemon').innerHTML += `Type 0 Pokemon: ${allPoke[0].types[0].type.name}<br>`;
-    document.getElementById('pokemon').innerHTML += `Type 1 Pokemon: ${allPoke[0].types[1].type.name}<br>`;
-    document.getElementById('pokemon').innerHTML += `Größe Pokemon: ${allPoke[0].height}<br>`;
-    document.getElementById('pokemon').innerHTML += `Gewicht Pokemon: ${allPoke[0].weight}<br>`;
-    document.getElementById('pokemon').innerHTML += `Spezies Pokemon: ${allPoke[0].species.name}<br>`;
-    document.getElementById('pokemon').innerHTML += `png-URL Pokemon: ${allPoke[0].sprites.front_shiny}<br>`;
-    document.getElementById('pokemon').innerHTML += `<img src="${allPoke[0].sprites.front_shiny}" alt="Bild Pokemon"><br>`;
-    document.getElementById('pokemon').innerHTML += `Bild Pokemon: ${allPoke[0].sprites.other.home.front_default}<br>`;
-    document.getElementById('pokemon').innerHTML += `<img src="${allPoke[0].sprites.other.home.front_default}" alt="Bild Pokemon"><br>`;
+function getInfo() {
+
+    // document.getElementById('pokemon').innerHTML = `Name Pokemon: ${allPoke[0].name}<br>`;
+    // document.getElementById('pokemon').innerHTML += `ID Pokemon: ${allPoke[0].id}<br>`;
+    // document.getElementById('pokemon').innerHTML += `Type 0 Pokemon: ${allPoke[0].types[0].type.name}<br>`;
+    // document.getElementById('pokemon').innerHTML += `Type 1 Pokemon: ${allPoke[0].types[1].type.name}<br>`;
+    // document.getElementById('pokemon').innerHTML += `Größe Pokemon: ${allPoke[0].height}<br>`;
+    // document.getElementById('pokemon').innerHTML += `Gewicht Pokemon: ${allPoke[0].weight}<br>`;
+    // document.getElementById('pokemon').innerHTML += `Spezies Pokemon: ${allPoke[0].species.name}<br>`;
+    // document.getElementById('pokemon').innerHTML += `png-URL Pokemon: ${allPoke[0].sprites.front_shiny}<br>`;
+    // document.getElementById('pokemon').innerHTML += `<img src="${allPoke[0].sprites.front_shiny}" alt="Bild Pokemon"><br>`;
+    // document.getElementById('pokemon').innerHTML += `Bild Pokemon: ${allPoke[0].sprites.other.home.front_default}<br>`;
+    // document.getElementById('pokemon').innerHTML += `<img src="${allPoke[0].sprites.other.home.front_default}" alt="Bild Pokemon"><br>`;
+    for ( index = startIndex; index < 13; index++) {
+            document.getElementById('overview_poke').innerHTML += returnPokemon(index);
+        
+    }
+
+
+    function returnPokemon(index) {
+        let arrayID = index-1;
+        return `
+            <div id="one_pokemon" class="one_pokemon">
+                <img class="img_poke" src="${allPoke[arrayID].sprites.other.home.front_default}" alt="">
+                <p class="id_poke">ID Pokemon: ${allPoke[arrayID].id}<br></p>
+                <p class="id_poke">Name: ${allPoke[0].name}<br></p>
+            </div>
+        `
+    };
 }
 
 function showPoke() {
