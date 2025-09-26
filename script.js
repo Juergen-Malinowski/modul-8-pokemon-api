2ace517b66aed6289a37e15cd4762faa142f4034
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-26 10:32:47 +0200

    new svg-files for pokemon-types

--- script.js
+++ script.js
@@ -12,14 +12,30 @@ async function loadPokemon() {
         console.log("Index-Nr. ist: ", index);
         console.log(pokeAsJson);
     }
-    console.log("all Pokemon: ", allPoke);
+    // console.log("alle Pokemon: ", allPoke);
 
     document.getElementById('pokemon').innerHTML = `Name Pokemon: ${allPoke[0].name}<br>`;
-
+    document.getElementById('pokemon').innerHTML += `ID Pokemon: ${allPoke[0].id}<br>`;
+    document.getElementById('pokemon').innerHTML += `Type 0 Pokemon: ${allPoke[0].types[0].type.name}<br>`;
+    document.getElementById('pokemon').innerHTML += `Type 1 Pokemon: ${allPoke[0].types[1].type.name}<br>`;
+    document.getElementById('pokemon').innerHTML += `height Pokemon: ${allPoke[0].height}<br>`;
+    document.getElementById('pokemon').innerHTML += `weight Pokemon: ${allPoke[0].weight}<br>`;
+    document.getElementById('pokemon').innerHTML += `species Pokemon: ${allPoke[0].species.name}<br>`;
+    document.getElementById('pokemon').innerHTML += `png-URL Pokemon: ${allPoke[0].sprites.front_shiny}<br>`;    
     // let type = pokeAsJson.types[0].type.name;
     // console.log(type);
     // document.getElementById('pokemon').innerHTML += pokeAsJson.types[0].type.name;
 };
+// "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/1.ogg"
+// "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/1.ogg"
+// loadPokemon();
 
+function showPoke() {
+    document.getElementById('show_poke').innerHTML = renderPoke();
+}
 
-loadPokemon();
+function renderPoke () {
+    return `
+    
+    `
+}
